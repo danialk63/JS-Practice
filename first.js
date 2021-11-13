9a5beed464527ce9e29865e2f19562d3e2792a19
Ahmad Khalid <danialkhalid63@gmail.com>
2021-11-13 14:23:15 +0500

    updated

--- first.js
+++ first.js
@@ -11,3 +11,47 @@ output.innerHTML = input.value;
 
 }
 
+// function handleAddNewTodo(){
+//     console.log("Handling new todo button");
+// }
+
+// function doBindings(){
+//     var btn = document.getElementById("btnAdd");
+//     btn.onclick = handleAddNewTodo;
+// }
+// window.onload = doBindings;
+
+function doBindings(){
+    var btn = document.getElementById("btnAdd");
+    btn.onclick = handleAddNewTodo;
+}
+window.onload = doBindings;
+
+
+
+//ToDo list m element add krna
+function handleAddNewTodo(){
+       // console.log("Handling new todo button");
+        var newToDo = document.getElementById("newToDo").value;
+        var todos = document.getElementById("todos");
+        var newTodoText = document.createTextNode(newToDo);
+        var newLi = document.createElement("li")
+        newLi.appendChild(newTodoText);
+        todos.appendChild(newLi);
+    }
+
+
+//ToDo list m element delete
+    function handleDelete(e){
+
+        var tag = e.target;
+        var li = tag.parentNode;
+        li.parentNode.removeChild(li);
+
+
+    }
+    
+  
+
+
+
